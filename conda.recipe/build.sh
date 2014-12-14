f85cbd05aa0df9e558f36c4be352182e009c56a9
Christine Doig <christine.doig.cardet@gmail.com>
2014-12-14 17:43:32 -0600

    bootstrap gradle project of focused crawler, some src fixes, dependency cleanup and conda recipe

---
+++ conda.recipe/build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ "$(uname)" == "Darwin" ]; then
+    export JAVA_HOME=$(/usr/libexec/java_home)
+    export JRE_HOME=${JAVA_HOME}/jre
+else
+    export JAVA_HOME="/usr/lib/jvm/java"
+    export JRE_HOME="/usr/lib/jvm/jre"
+fi
+
+#BLD_DIR=`pwd`
+
+#SRC_DIR=$RECIPE_DIR/..
+#pushd $SRC_DIR
+
+mkdir -vp ${PREFIX}/bin;
+mkdir -vp ${PREFIX}/lib;
+
+# build focused_crawler
+
+export TERM=${TERM:-dumb} 
+./gradlew clean installApp --stacktrace
+
+echo `pwd`
+pushd build/install/focused_crawler
+cp -r bin/ ${PREFIX}/bin
+cp -r lib/ ${PREFIX}/lib
