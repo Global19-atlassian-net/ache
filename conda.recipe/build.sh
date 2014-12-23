c9e64dc66b12b22d0be1eff5af4cd17b04451770
Christine Doig <christine.doig.cardet@gmail.com>
2014-12-22 23:06:10 -0600

    fix conda recipe again...

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -13,6 +13,7 @@
 #SRC_DIR=$RECIPE_DIR/..
 #pushd $SRC_DIR
 
+mkdir -vp ${PREFIX}/bin;
 mkdir -vp ${PREFIX}/lib/ache/bin;
 mkdir -vp ${PREFIX}/lib/ache/lib;
 
@@ -26,3 +27,6 @@ cp -r bin/* ${PREFIX}/lib/ache/bin/
 cp -r lib/* ${PREFIX}/lib/ache/lib/
 
 ln -s "${PREFIX}/lib/ache/bin/ache" "${PREFIX}/bin/ache"
+
++ ln -s /Users/cdoig/anaconda/envs/_build/lib/ache/bin/ache /Users/cdoig/anaconda/envs/_build/bin/ache
+ln: /Users/cdoig/anaconda/envs/_build/bin/ache: No such file or directory
